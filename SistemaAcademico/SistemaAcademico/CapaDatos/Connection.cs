ea32c6c3176ab2438da3daf64407582826244a47
christianv07 <christianvasquezmelo@gmail.com>
2016-07-16 13:39:30 -0400

    2

---
+++ SistemaAcademico/SistemaAcademico/CapaDatos/Connection.cs
@@ -0,0 +1,8 @@
+﻿
+namespace SistemaAcademico.CapaDatos
+{
+    public class Connection
+    {
+        public static string  con = "Data Source =.; Initial catalog = db_final; Integrated Security = True";
+    }
+}
