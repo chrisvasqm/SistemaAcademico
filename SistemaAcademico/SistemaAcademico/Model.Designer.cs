f91382f9dfcfc82f953e9cf64d8578eeded40b78
chrisvasqm <christianvasquezmelo@gmail.com>
2021-11-25 21:03:17 -0400

    Update dependencies

--- SistemaAcademico/SistemaAcademico/Model.Designer.cs
+++ SistemaAcademico/SistemaAcademico/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\CHRIS\Dropbox\Development\Visual Studio\Projects\sysacademico\SistemaAcademico\SistemaAcademico\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\DominicoAmericano\Visual Studio\Projects\sysacademico\SistemaAcademico\SistemaAcademico\Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
