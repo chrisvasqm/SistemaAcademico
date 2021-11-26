3790c188480a34fa67f81b3d86ea823a4d70cef4
chrisvasqm <christianvasquezmelo@gmail.com>
2021-11-25 21:01:38 -0400

    Rearrange columns

--- SistemaAcademico/SistemaAcademico/PL/FormStudentPanel.cs
+++ SistemaAcademico/SistemaAcademico/PL/FormStudentPanel.cs
@@ -24,11 +24,10 @@ namespace PL
 
             dgv1.DataSource = _studentService.GetRecordsByMatricula(lblMatricula.Text);
             dgv1.Columns["ID"].Visible = false;
-            dgv1.Columns["Student"].Visible = true;
+            dgv1.Columns["Student"].Visible = false;
             dgv1.Columns["Materia"].Width = 150;
             dgv2.DataSource = _studentService.GetAllSubject();
             dgv2.Columns["ID"].Visible = false;
-            dgv2.Columns["isAproved"].Visible = false;
             dgv2.Columns["Materias"].Width = 150;
         }
         private void btnAtras_Click(object sender, EventArgs e)
@@ -55,7 +54,7 @@ namespace PL
             else 
                 _studentService.SetStudentSubject(selectionValues);
 
-            _studentService.GetRecordsByMatricula(lblMatricula.Text);
+            dgv1.DataSource = _studentService.GetRecordsByMatricula(lblMatricula.Text);
         }
     }
 }
